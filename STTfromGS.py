b91afdfa929aaf813fdc560a41a0fb1679cf07d9
tmyoas <tmyoas@gmail.com>
2018-08-25 02:54:23 +0900

    Get recognition_config from file

--- STTfromGS.py
+++ STTfromGS.py
@@ -1,29 +1,30 @@
 import datetime
 import os
+import re
 import argparse
 import codecs
 from google.cloud import speech_v1p1beta1
 from google.cloud.speech_v1p1beta1 import enums
 from google.cloud.speech_v1p1beta1 import types
 
-def transcribe_gcs(gcs_uri, hint_phrases):
+def transcribe_gcs(gcs_uri, hint_phrases, config):
     """Asynchronously transcribes the audio file specified by the gcs_uri."""
     client = speech_v1p1beta1.SpeechClient()
 
     audio = types.RecognitionAudio(uri=gcs_uri)
     config = types.RecognitionConfig(
-        encoding=enums.RecognitionConfig.AudioEncoding.FLAC,
-        sample_rate_hertz=16000,
-        language_code='en-US',
-        enable_automatic_punctuation=True,
-        enable_speaker_diarization=True,
-        diarization_speaker_count=3,
+        encoding=config['encoding'],
+        sample_rate_hertz=config['sample_rate_hertz'],
+        language_code=config['language_code'],
+        enable_automatic_punctuation=config['enable_automatic_punctuation'],
+        enable_speaker_diarization=config['enable_speaker_diarization'],
+        diarization_speaker_count=config['diarization_speaker_count'],
         speech_contexts=[speech_v1p1beta1.types.SpeechContext(phrases=hint_phrases)])
 
     operation = client.long_running_recognize(config, audio)
 
     print('Waiting for operation to complete...')
-    response = operation.result(timeout=9000)
+    response = operation.result(timeout=900)
     result_max_index = len(response.results) - 1
 
     timestamp = datetime.datetime.today().strftime("%Y%m%d-%H%M%S")
@@ -52,14 +53,26 @@ def get_stringlist_from_file(path):
     return str_list
 
 def get_hashmap_from_file(path):
-    # TODO: Convert each line(aaa=bbb) in file to hashmap({'aaa':'bbb'})
+    # Default values
     hashmap = {}
+    hashmap['encoding'] = enums.RecognitionConfig.AudioEncoding.FLAC
+    hashmap['sample_rate_hertz'] = 16000
+    hashmap['language_code'] = 'en-US'
+    hashmap['enable_automatic_punctuation'] = True
+    hashmap['enable_speaker_diarization'] = False
+    hashmap['diarization_speaker_count'] = 1
+
+    f = open(path, 'r')
+    for line in f:
+        tmp = line.rstrip().split("=")
+        if tmp[0] == 'encoding' or tmp[0].count('enable'):
+            hashmap[tmp[0]] = eval(tmp[1])
+        elif tmp[1].isdecimal():
+            hashmap[tmp[0]] = int(tmp[1])
+        else:
+            hashmap[tmp[0]] = tmp[1]
+    f.close()
     return hashmap
-
-def get_config_from_hashmap(config):
-    # TODO: Fill shortage config and return types.RecognitionConfig()
-    return types.RecognitionConfig()
-
 
 if __name__ == '__main__':
     # Setting of command-line parameters
@@ -69,14 +82,11 @@ if __name__ == '__main__':
     HINT_FILE = './resources\hint_list'
     hints = []
     if os.path.isfile(HINT_FILE):
-        hints = get_Stringlist_from_file(HINT_FILE)
+        hints = get_stringlist_from_file(HINT_FILE)
 
-    # CONFIG_FILE = './resources\config'
-    # config = {}
-    # if os.path.isfile(CONFIG_FILE):
-    #     config = get_Stringlist_from_file(CONFIG_FILE)
-    # if len(config) == 0:
-    # TODO: Handling if config file is shortage
-        
+    CONFIG_FILE = './resources/recognition_config'
+    config = {}
+    if os.path.isfile(CONFIG_FILE):
+        config = get_hashmap_from_file(CONFIG_FILE)
+
     transcribe_gcs(parser.parse_args().gspath, hints, config)
-
