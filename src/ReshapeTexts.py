aef8f283708932ddf24dbc3417672a1e2bb1542f
tmyoas <tmyoas@gmail.com>
2018-10-02 00:28:06 +0900

    beta

--- src/ReshapeTexts.py
+++ src/ReshapeTexts.py
@@ -10,51 +10,56 @@ import re
 def replace_patterns(del_pattern, ins_pattern, input_list):
     temp_list = []
     for i in range(0, len(input_list) - 1):
-        temp_list[i] = re.sub(del_pattern, ins_pattern, input_list[i])
+        temp_list.append(re.sub(del_pattern, ins_pattern, input_list[i]))
     while '' in temp_list:
         temp_list.remove('')
     return temp_list
 
 def remove_additional_info(input_list):
+    # remove "Transcript:[ ]*", "Confidence: [0-9.]\n" for STTfromGS.py
     pattern = r'transcript:[ ]*|confidence: [0-9.]\n|[0-9]{2}:[0-9]{2}:[0-5][0-9].?'
     return replace_patterns(pattern, '', input_list)
 
 def remove_line_feed_code(input_list):
+    # DONE remove "\r" and "\n"
     temp_list = []
     for i in range(0, len(input_list) - 1):
-        temp_list[i] = input_list[i].strip()
+        temp_list.append(input_list[i].strip())
     while '' in temp_list:
         temp_list.remove('')
     return temp_list
 
 def remove_space_around_apostrophe(input_list):
+    # remove <sp> around "'"
     del_pattern = r" '|' "
     ins_pattern = r"'"
-    replace_patterns(del_pattern, ins_pattern, input_list)
-    return input_list
+    return replace_patterns(del_pattern, ins_pattern, input_list)
 
-def replace_delimiter(input_list):
-    return input_list
+def replace_space(input_list):
+    # replace [ ]+ to " "
+    del_pattern = r'[ ]+'
+    ins_pattern = ' '
+    return replace_patterns(del_pattern, ins_pattern, input_list)
 
-def remove_punctuation():
-    return input_list
+def remove_punctuation(input_list):
+    # DONE also remove ",", ".", "!", and "?"
+    pattern = r"[,.!?]+."
+    return replace_patterns(pattern, '', input_list)
 
 def get_reshaped_texts(input_list):
     # TODO:
-    # DONE unify upper / lower case.
-    # DONE remove "Transcript:[ ]*", "Confidence: [0-9.]\n" for STTfromGS.py
-    # DONE remove "\r" and "\n". : strip?
-    # DONE remove <sp> around "'". : [ ]*'[ ]* -> '
-    # replace "-" -> split("-")
-    # replace [ ]+ to " " : split(" ") -> if list[n] == "" : del list[n] ?
-    # also remove ",", ".", "!", and "?"
+    # how to change "-"
 
+    # unify upper / lower case.
     for i in range(0, len(input_list) - 1):
-        input_list[i] = input_list.lower()
+        input_list[i] = input_list[i].lower()
 
-    list = []
-
-    return ' '.join(list)
+    ans_list = remove_additional_info(input_list)
+    ans_list = remove_line_feed_code(ans_list)
+    ans_list = remove_space_around_apostrophe(ans_list)
+    ans_list = replace_space(ans_list)
+    ans_list = remove_punctuation(ans_list)
+    return ' '.join(ans_list)
 
 def make_output_file(parser):
     return 0
@@ -65,7 +70,7 @@ if __name__ == '__main__':
     # target: A file that you want to reshape
     # file: output filepath and filename
     parser = argparse.ArgumentParser()
-    parser.add_argument('target', nargs='1', help='A reshape-target file.')
+    parser.add_argument('target', help='A reshape-target file.')
     parser.add_argument('output', nargs='?', help='An output file (default: <target filename>_reshaped.txt)')
 
     fin = open(parser.parse_args().target, encoding="utf8")
@@ -73,6 +78,7 @@ if __name__ == '__main__':
     fin.close()
 
     # reshape_texts
+    output = get_reshaped_texts(input_list)
 
     # TODO: separate method (make_output_file)
     if not parser.parse_args().output:
@@ -81,8 +87,8 @@ if __name__ == '__main__':
     else:
         outputpath= os.path.split(parser.parse_args().output)
     os.makedirs(outputpath[0], exist_ok=True)
-    file = open(parser.parse_args().output, mode='w', encoding="utf8")
-    # TODO: output in file
-    file.close()
+    fout = open(os.path.join(outputpath[0], outputpath[1]), mode='w', encoding="utf8")
+    fout.write(output)
+    fout.close()
 
 
